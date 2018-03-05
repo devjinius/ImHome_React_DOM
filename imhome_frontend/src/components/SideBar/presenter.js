994b1027dfe529b56af2b6ef560d817d392c1431
seonhyungjo <senjing9379@gmail.com>
2018-03-05 14:51:00 +0900

    add icons

--- imhome_frontend/src/components/SideBar/presenter.js
+++ imhome_frontend/src/components/SideBar/presenter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./styles.scss";
-import FaBeer from 'react-icons/fa/beer';
+import MdReceipt from 'react-icons/lib/md/receipt'
 
 const SideBar = () => {
   return (
@@ -9,26 +9,25 @@ const SideBar = () => {
       <div className={styles.sidebar_dropdown}>
         <a>메뉴</a>
       </div>
-      <h3> Lets go for a <FaBeer />? </h3>
       <div className={styles.sidebar_inner}>
         <ul className={styles.navi}>
           <li className={styles.leftBtn} id="imhome">
-            
+            <MdReceipt size={40} color={'#000'} /> 아임홈
           </li>
           <li className={styles.leftBtn} id="dongwonMain">
-              동원 F&#38;B
+          <MdReceipt size={40} color={'#000'}/> 동원 F&#38;B
           </li>
 
           <li className={styles.leftBtn} id="costcoMain">
-              코스트코
+          <MdReceipt size={40} color={'#000'}/>  코스트코
           </li>
 
           <li className={styles.leftBtn} id="wellMain">
-              웰푸드
+          <MdReceipt size={40} color={'#000'}/>  웰푸드
           </li>
 
           <li className={styles.leftBtn} id="internetMain">
-              인터넷주문
+          <MdReceipt size={40} color={'#000'}/>  인터넷주문
           </li>
         </ul>
       </div>
