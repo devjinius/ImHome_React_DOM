6b96f3e7866f51f071fa0b9182e1b3f5a1d39c4a
eugene94 <eugenekang94@gmail.com>
2018-07-25 17:34:48 +0900

    Modify Register

--- src/components/App/presenter.js
+++ src/components/App/presenter.js
@@ -3,20 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 import Header from 'components/Header';
 import Nav from 'components/Navigator';
 
-//회원가입
-// import {
-//   RegisterButton,
-//   RegisterInput,
-//   HeadText,
-//   SubText,
-//   ItemText,
-//   BigLine,
-//   MiddleLine,
-//   HeaderBackground,
-//   Logo,
-//   Main,
-//   HeaderDiv
-// } from "common/register"
+// 회원가입;
+import {
+  RegisterButton,
+  RegisterInput,
+  HeadText,
+  SubText,
+  ItemText,
+  BigLine,
+  MiddleLine,
+  HeaderBackground,
+  Logo,
+  Main,
+  HeaderDiv,
+  RegTop,
+  RegMain,
+  RegRow,
+  ButtonText
+} from 'common/register';
 
 //메인페이지
 // import {
@@ -33,29 +37,86 @@ import Nav from 'components/Navigator';
 //   Tr
 // } from "common/main"
 
-import { MainDiv, MainText, Button, SubText } from 'common/userDel';
+// import { MainDiv, MainText, Button, SubText } from 'common/userDel';
 
 const App = props => {
-  // return (
-  //   // 회원가입 부분
-  //      <Main>
-  //     <HeaderDiv>
-  //       <HeaderBackground>
-  //         <Logo />
-  //       </HeaderBackground>
-  //     </HeaderDiv>
-  //     <BigLine />
-  //     <RegisterInput place />
-  //     <HeadText>headText</HeadText>
-  //     <SubText>subText</SubText>
-  //     <ItemText>itemText</ItemText>
-  //     <ItemText>itemText</ItemText>
-  //     <ItemText>itemText</ItemText>
-  //     <ItemText>itemText</ItemText>
-  //     <MiddleLine />
-  //     <RegisterButton />
-  //   </Main>
-  // )
+  return (
+    // 회원가입 부분
+    <Main>
+      <HeaderDiv>
+        <HeaderBackground>
+          <Logo />
+        </HeaderBackground>
+      </HeaderDiv>
+
+      <BigLine />
+
+      <RegTop>
+        <div>
+          <HeadText>가입 정보 입력</HeadText>
+        </div>
+        <div>
+          <SubText>로그인 정보 및 가입 정보를 입력하세요</SubText>
+        </div>
+      </RegTop>
+      <RegMain>
+        <MiddleLine />
+        <RegRow>
+          <ItemText>사용하실 아이디</ItemText>
+          <RegisterInput placeholder={'아이디'} />
+        </RegRow>
+        <RegRow>
+          <ItemText>비밀번호</ItemText>
+          <RegisterInput placeholder={'비밀번호(8자 이상)'} />
+        </RegRow>
+        <RegRow>
+          <ItemText>비밀번호 재확인</ItemText>
+          <RegisterInput placeholder={'비밀번호 재확인'} />
+        </RegRow>
+      </RegMain>
+
+      <RegMain>
+        <MiddleLine />
+
+        <RegRow>
+          <ItemText>대표 성함</ItemText>
+          <RegisterInput placeholder={'대표 성함'} />
+        </RegRow>
+        <RegRow>
+          <ItemText>사업자 번호</ItemText>
+          <RegisterInput placeholder={'사업자 번호'} />
+        </RegRow>
+        <RegRow>
+          <ItemText>사업장 주소</ItemText>
+          <RegisterInput placeholder={'사업장 주소'} />
+        </RegRow>
+        <RegRow>
+          <ItemText>회사명</ItemText>
+          <RegisterInput placeholder={'사업장 등록증 상의 회사명'} />
+        </RegRow>
+        <RegRow>
+          <ItemText>이메일 주소</ItemText>
+          <RegisterInput
+            placeholder={'세금계산서 받으실 수 있는 이메일 주소'}
+          />
+        </RegRow>
+        <RegRow>
+          <ItemText>휴대폰 번호</ItemText>
+          <RegisterInput placeholder={'휴대폰 번호'} />
+        </RegRow>
+        <RegRow>
+          <ItemText>지점명</ItemText>
+          <RegisterInput placeholder={'지점명'} />
+        </RegRow>
+
+        <MiddleLine />
+
+        <RegisterButton>
+          <ButtonText>가입하기</ButtonText>
+        </RegisterButton>
+      </RegMain>
+    </Main>
+  );
   // 메인 페이지
   // return [
   //   <HeaderDiv>
@@ -111,20 +172,20 @@ const App = props => {
   //   </Main>
   // ]
   //회원삭제
-  return [
-    <MainDiv>
-      <div>
-        <MainText>회원목록에서 삭제 하시겠습니까?</MainText>
-      </div>
-      <div>
-        <Button>취소하기</Button>
-        <Button>삭제하기</Button>
-      </div>
-      <div>
-        <SubText>삭제가 되면 복구가 되지 않습니다.</SubText>
-      </div>
-    </MainDiv>
-  ];
+  // return [
+  //   <MainDiv>
+  //     <div>
+  //       <MainText>회원목록에서 삭제 하시겠습니까?</MainText>
+  //     </div>
+  //     <div>
+  //       <Button>취소하기</Button>
+  //       <Button>삭제하기</Button>
+  //     </div>
+  //     <div>
+  //       <SubText>삭제가 되면 복구가 되지 않습니다.</SubText>
+  //     </div>
+  //   </MainDiv>
+  // ];
   // return [<Header />, <Nav />]
 };
 
--- src/styledCom/register.js
+++ src/styledCom/register.js
@@ -1,17 +1,17 @@
-import styled, { injectGlobal } from "styled-components"
-import WebFont from "webfontloader"
-import backgroundImage from "img/register/bg.png"
-import headerBackground from "img/register/up_bg.png"
-import logo from "img/register/logo.png"
+import styled, { injectGlobal } from 'styled-components';
+import WebFont from 'webfontloader';
+import backgroundImage from 'img/register/bg.png';
+import headerBackground from 'img/register/up_bg.png';
+import logo from 'img/register/logo.png';
 
 /**
  * Google Font 호출해서 사용을 위한 webfont추가
  */
 WebFont.load({
   google: {
-    families: ["Nanum+Gothic:400,700,800", "sans-serif"]
+    families: ['Nanum+Gothic:400,700,800', 'sans-serif']
   }
-})
+});
 
 /**
  * body적용을 위한 css
@@ -25,7 +25,7 @@ injectGlobal`
         margin: 0px;
         background-size: 100%;
     }
-`
+`;
 
 /**
  * Header Div
@@ -33,7 +33,7 @@ injectGlobal`
 export const HeaderDiv = styled.div`
   width: 100%;
   height: 225px;
-`
+`;
 
 /**
  * Header BackGround
@@ -42,7 +42,10 @@ export const HeaderBackground = styled.div`
   background-image: url(${headerBackground});
   width: 100%;
   height: 100%;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 /**
  * Logo
@@ -51,7 +54,7 @@ export const Logo = styled.div`
   background-image: url(${logo});
   width: 300px;
   height: 106px;
-`
+`;
 
 /**
  * Main Div
@@ -61,59 +64,91 @@ export const Main = styled.div`
   width: 1026px;
   height: 1538px;
   margin: 0 auto;
-`
+`;
+
+/**
+ * RegTop
+ */
+export const RegTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 150px;
+`;
+
+/**
+ * RegMain Div
+ */
+export const RegMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+/**
+ * RegRow Div
+ * 입력창 각각 한줄
+ */
+export const RegRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 800px;
+  padding: 0px 50px;
+  margin-bottom: 10px;
+`;
 
 /**
  * 가입하기 버튼
  */
 export const RegisterButton = styled.button`
-  font-family: 'Nanum Gothic', sans-serif;
-  font-weight: 800;
   width: 200px;
   height: 50px;
   background-color: #ffffff;
   border: solid 2px #fe4c8d;
   border-radius: 3px;
-`
+`;
 
 /**
- * 회원가입 Default Input 
+ * 회원가입 Default Input
  */
 export const RegisterInput = styled.input`
   font-family: 'Nanum Gothic', sans-serif;
   width: 625px;
   height: 51px;
   border: solid 3px #c2c2c2;
-`
+`;
 
 /**
- * 회원가입 Main Text 
+ * 회원가입 Main Text
  */
 export const HeadText = styled.span`
   font-family: 'Nanum Gothic', sans-serif;
   font-weight: bold;
   color: #000000;
   font-size: 34px;
-`
+`;
 
 /**
- * 회원가입 Sub Text 
+ * 회원가입 Sub Text
  */
 export const SubText = styled.span`
   font-family: 'Nanum Gothic', sans-serif;
   font-weight: bold;
   color: #707070;
   font-size: 24px;
-`
+`;
 
 /**
- * 회원가입 Question Text 
+ * 회원가입 Question Text
  */
 export const ItemText = styled.span`
   font-family: 'Nanum Gothic', sans-serif;
   color: #000000;
   font-size: 18px;
-`
+`;
 
 /**
  * 큰 크기 Line
@@ -121,8 +156,8 @@ export const ItemText = styled.span`
 export const BigLine = styled.hr`
   width: 1022px;
   border: solid 2px #c2c2c2;
-  margin: 0;
-`
+  margin-bottom: 50px;
+`;
 
 /**
  * 중간 크기 Line
@@ -130,5 +165,15 @@ export const BigLine = styled.hr`
 export const MiddleLine = styled.hr`
   width: 900px;
   border: solid 2px #c2c2c2;
-  margin: 0;
-`
+  margin: 50px 0px;
+`;
+
+/**
+ * ButtonText
+ */
+export const ButtonText = styled.span`
+  font-family: 'Nanum Gothic ExtraBold', sans-serif;
+  font-weight: bold;
+  font-size: 22px;
+  color: #fe4c8d;
+`;
